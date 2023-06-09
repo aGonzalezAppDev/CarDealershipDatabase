942df6cc2f420b023addd7c2362582de6e1de9bc
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-06-09 12:02:00 -0400

    finished the tables, inputing data

--- creatingDealershipDatabase.sql
+++ creatingDealershipDatabase.sql
@@ -1,26 +1,69 @@
 DROP DATABASE IF EXISTS cardealershipdatabase;
 CREATE DATABASE cardealershipdatabase;
 
+-- Table 1
 CREATE TABLE dealerships (
 	dealership_id  int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name varchar(50),
-    address varchar(50),
-    phone varchar(12)
+    dealership_name varchar(50) NOT NULL,
+    address varchar(50) NOT NULL,
+    phone varchar(12) NOT NULL
 );
+INSERT INTO dealerships
+VALUES (1, "Best Dealership", "123 Memorial Drive, Hackensack, NJ","973-546-7656");
+INSERT INTO dealerships
+VALUES (2, "Okay Dealership", "453 Paramus Ava, Paramus, NJ", "201-854-0926");
 
 
-CREATE TABLE vehicles(
+-- Table 2
+CREATE TABLE vehicles (
 	VIN int PRIMARY KEY NOT NULL,
-    model varchar(50),
-    make varchar(50),
-    color varchar(50),
-    year int ,
+    make varchar(50) NOT NULL,
+    model varchar(50) NOT NULL,
+    color varchar(50) NOT NULL,
+    year int NOT NULL,
     vehicleType varchar(50),
-    odometer int,
-    price int
+    odometer int NOT NULL,
+    price int NOT NULL,
+    sold bool NOT NULL
+);
+INSERT INTO vehicles
+VALUES(10201, "Toyota", "Camry","silver",2015,"sedan",10000,12000, 0);
+INSERT INTO vehicles
+VALUES(10202,"Kia","Stinger","black",2020, "sedan",5000,60000, 1);
+INSERT INTO vehicles
+VALUES (10203,"Chevy","Silverado", "blue",2018, "truck",12000,40000,0);
+
+-- Table 3
+CREATE TABLE inventory (
+	dealership_id int,
+    VIN int
 );
 
+-- Table 4
+CREATE TABLE sales_contracts (
+	sales_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (VIN) REFERENCES vehicles(VIN),
+    customer_name varchar(50) NOT NULL,
+    sales_coordinator varchar(50) NOT NULL,
+    FOREIGN KEY (price) REFERENCES vehicles(price),
+    FOREIGN KEY (year) REFERENCES vehicles(year),
+    FOREIGN KEY (make) REFERENCES vehicles(make),
+    FOREIGN KEY (model) REFERENCES vehicles(model),
+    FOREIGN KEY (color) REFERENCES vehicles(color)
+);
 
+-- Table 5
+CREATE TABLE lease_contracts (
+	lease_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (VIN) REFERENCES vehicles(VIN),
+    customer_name varchar(50) NOT NULL,
+    lease_coordinator varchar(50) NOT NULL,
+    FOREIGN KEY (price) REFERENCES vehicles(price),
+    FOREIGN KEY (year) REFERENCES vehicles(year),
+    FOREIGN KEY (make) REFERENCES vehicles(make),
+    FOREIGN KEY (model) REFERENCES vehicles(model),
+    FOREIGN KEY (color) REFERENCES vehicles(color)
+);
 
 
 
